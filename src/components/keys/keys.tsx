60626629736789b31d94949bbcc1280909f132ae
Surprised Pika <surprisedpikaaaaaaa@gmail.com>
2024-06-30 23:33:07 +0100

    Log Unknown keys

--- src/components/keys/keys.tsx
+++ src/components/keys/keys.tsx
@@ -7,6 +7,7 @@ import styles from "./styles.module.scss";
 
 import Key from "@/components/key/key";
 import { LayoutData } from "@/components/wrapper/wrapper";
+import { listen } from "@tauri-apps/api/event";
 
 interface props {
   layout: LayoutData | undefined;
@@ -14,16 +15,26 @@ interface props {
 
 export default function Keys(props: props) {
   const [pressedKeys, setPressedKeys] = useState<Set<string>>(new Set());
+  const [unknownKey, setUnknownKey] = useState("");
 
   useEffect(() => {
     const syncKeys = () => {
       invoke<string[]>("keys").then((keys) => {
+        keys.forEach(key => {
+          if (key.includes("Unknown")) {
+            setUnknownKey(key);
+          }
+        })
         setPressedKeys(new Set(keys));
         if (keys.length > 0) {
           console.log(keys);
         }
       });
     };
+
+    listen<string>('unknownKey', (payload) => {
+      setUnknownKey(payload.payload);
+    });
 
     const syncClock = setInterval(syncKeys, 50);
 
@@ -48,6 +59,7 @@ export default function Keys(props: props) {
             />
           );
         })}
+        {unknownKey && <p className={styles.warning}>Unknown Key: <code>{unknownKey}</code></p>}
     </div>
   );
 }
