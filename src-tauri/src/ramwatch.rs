5b5fedfe891d1e341d12bb81387244a1759ae281
Surprised Pika <surprisedpikaaaaaaa@gmail.com>
2025-03-27 16:27:03 +0000

    rename variables

--- src-tauri/src/ramwatch.rs
+++ src-tauri/src/ramwatch.rs
@@ -3,20 +3,28 @@ use proc_mem::{ ProcMemError, Process };
 #[tauri::command]
 pub async fn main() {
     println!("ERM");
-    let test: Result<Process, ProcMemError> = Process::with_name("ItTakesTwo.exe");
+    let process_res: Result<Process, ProcMemError> = Process::with_name("ItTakesTwo.exe");
     loop {
-        if let Ok(ref paul) = test {
-            let p: Result<bool, ProcMemError> = paul.read_mem::<bool>(
+        if let Ok(ref process) = process_res {
+            let memory_res: Result<bool, ProcMemError> = process.read_mem::<bool>(
                 // bool isLoading: "ItTakesTwo.exe", 0x07A07A60, 0x180, 0x2b0, 0x0, 0x458, 0xf9;
-                paul
+                process
                     .read_ptr_chain(
-                        vec![paul.process_base_address, 0x07a07a60, 0x180, 0x2b0, 0x0, 0x458, 0xf9]
+                        vec![
+                            process.process_base_address,
+                            0x07a07a60,
+                            0x180,
+                            0x2b0,
+                            0x0,
+                            0x458,
+                            0xf9
+                        ]
                     )
                     .unwrap()
             );
 
-            if let Ok(craig) = p {
-                println!("{}", craig);
+            if let Ok(memory) = memory_res {
+                println!("{}", memory);
             }
         }
     }
