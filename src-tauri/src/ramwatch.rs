64507434022abd1067d52471025c91598de61c5d
Surprised Pika <surprisedpikaaaaaaa@gmail.com>
2025-03-27 16:44:39 +0000

    Improve debugging and make steps towards abstraction

--- src-tauri/src/ramwatch.rs
+++ src-tauri/src/ramwatch.rs
@@ -3,29 +3,36 @@ use proc_mem::{ ProcMemError, Process };
 #[tauri::command]
 pub async fn main() {
     println!("ERM");
-    let process_res: Result<Process, ProcMemError> = Process::with_name("ItTakesTwo.exe");
+
+    // bool isLoading: "ItTakesTwo.exe", 0x07A07A60, 0x180, 0x2b0, 0x0, 0x458, 0xf9;
+    type MemoryType = bool;
+    let process_name = "ItTakesTwo.exe";
+    let ptr_chain = vec![0x07a07a60, 0x180, 0x2b0, 0x0, 0x458, 0xf9];
+
+    let process_res: Result<Process, ProcMemError> = Process::with_name(process_name);
     loop {
         if let Ok(ref process) = process_res {
-            let memory_res: Result<bool, ProcMemError> = process.read_mem::<bool>(
-                // bool isLoading: "ItTakesTwo.exe", 0x07A07A60, 0x180, 0x2b0, 0x0, 0x458, 0xf9;
-                process
-                    .read_ptr_chain(
-                        vec![
-                            process.process_base_address,
-                            0x07a07a60,
-                            0x180,
-                            0x2b0,
-                            0x0,
-                            0x458,
-                            0xf9
-                        ]
-                    )
-                    .unwrap()
-            );
+            let ptr_chain_res = process.read_ptr_chain({
+                let mut chain = vec![process.process_base_address];
+                chain.extend(ptr_chain.clone());
+                chain
+            });
 
-            if let Ok(memory) = memory_res {
-                println!("{}", memory);
+            if let Ok(ptr_chain) = ptr_chain_res {
+                let memory_res: Result<MemoryType, ProcMemError> = process.read_mem::<MemoryType>(
+                    ptr_chain
+                );
+
+                if let Ok(memory) = memory_res {
+                    println!("{}", memory);
+                } else {
+                    println!("Error reading memory!");
+                }
+            } else {
+                println!("Error in pointer chain!");
             }
+        } else {
+            println!("Error in reading process!");
         }
     }
 }
